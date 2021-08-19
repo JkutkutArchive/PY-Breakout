c9d0a26bba991017d535996e1012a3edcafd175f
= <=>
2021-08-19 19:37:43 +0200

    ball added

--- main.py
+++ main.py
@@ -2,26 +2,43 @@
 import pygame; # library to generate the graphic interface
 import time; # to set a delay between each iteration
 from Classes.color import color;
+from Classes.ball import Ball;
 
 pygame.init() # Init pygame
 pygame.display.set_caption("Breakout") # Set the title of the game
 
 # CONSTANTS
 width, height = 1000, 1000 
-sizeX, sizeY = 50, 50 # Number of cell spots in each axis
-sizeWidthX = width / sizeX # Size of each spot
-sizeWidthY = height / sizeY
+# sizeX, sizeY = 50, 50 # Number of cell spots in each axis
+# sizeWidthX = width / sizeX # Size of each spot
+# sizeWidthY = height / sizeY
 COLOR = color() # Get the color class with the constants
 
 screen = pygame.display.set_mode((width, height)) # Set the size of the window
 
+# VARIABLES
+ball = Ball(500, 500)
+
+screen.fill(COLOR.BG) # Clean screen
 gameRunning = True # If false, the game stops
 timeRunning = False # If true, time runs (so iterations occur)
 while gameRunning:
-    screen.fill(COLOR.BG) # Clean screen
+    # screen.fill(COLOR.BG) # Clean screen
     
     if timeRunning:
-        time.sleep(0.1) # set a delay between each iteration
+        time.sleep(0.02) # set a delay between each iteration
+
+    # Update the ball
+    pygame.draw.circle(screen, COLOR.BG, ball.pos(), ball.size())
+    ball.move()
+    pygame.draw.circle(screen, ball.color(), ball.pos(), ball.size())
+
+    if ball.getX() < 0 or ball.getX() > width:
+        ball.bounce(x=true)
+    if ball.getY() < 0 or ball.getY() > width:
+        ball.bounce(y=true)
+
+    # Update the screen
     pygame.display.flip() # Update the screen
 
     for event in pygame.event.get(): # for each event
@@ -47,6 +64,6 @@ while gameRunning:
             if event.key == 32: # Space pressed
                 timeRunning = not timeRunning # Toggle the run of iterations
 
-print("Thanks for playing, I hope you liked it.")
+print("\nThanks for playing, I hope you liked it.")
 print("See more projects like this one on https://github.com/jkutkut/")
 pygame.quit() # End the pygame
