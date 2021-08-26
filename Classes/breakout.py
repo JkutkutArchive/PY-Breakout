7b5c2a56ead82c0a4207d83e28ae7df3fc8f499a
= <=>
2021-08-26 16:52:29 +0200

    All logic of the game now is stored here

--- Classes/breakout.py
+++ Classes/breakout.py
@@ -1,6 +1,10 @@
+import time
 import pygame
 from Classes.color import color
-from levelLoader.levelLoader import *
+from Classes.brick import *
+from Classes.player import Player
+from Classes.ball import Ball
+from levelLoader.levelLoader import setup, loadLevel
 
 class Breakout():
     # UI properties
@@ -34,23 +38,69 @@ class Breakout():
 
         self.resize(width, height)
 
+        setup(Breakout)
+
+        self.loadNextLevel()
+
         self.loop()
 
     def resize(self, width=None, height=None):
-        if width:
+        if width != None:
             Breakout.width = width
-        if height:
+            # update brick size
+            unit = width // 30
+            brick.width = unit
+            brick.height = unit // 2
+
+            # update player size
+            Player.unit = width // 50
+
+            # update ball size
+            Ball.radius = 5
+        
+        if height != None:
             Breakout.height = height
         
         Breakout.screen = pygame.display.set_mode((Breakout.width, Breakout.height))
+
     
     def loadNextLevel(self):
-        Breakout.currentLvl += 1
-        Breakout.player, Breakout.ball, Breakout.bricks = loadLevel(Breakout.currentLvl)
+        Breakout.player, Breakout.ball, Breakout.bricks = loadLevel(Breakout.currentLvl + 1)
+        Breakout.currentLvl = (Breakout.currentLvl + 1) % 3
 
 
     def loop(self):
         while Breakout.gameRunning:
+            time.sleep(0.04) # set a delay between each iteration
+            if Breakout.timeRunning:
+                # Update the ball
+                Breakout.ball.move()
+
+                if Breakout.player.inRange(Breakout.ball):
+                    Breakout.player.makeBallBounce(Breakout.ball)
+
+                # Check bricks
+                bricksDestroyed = set()
+                for b in Breakout.bricks:
+                    b.attemptHit(Breakout.ball)
+                    if b.destroyed():
+                        bricksDestroyed.add(b)
+                
+                Breakout.bricks -= bricksDestroyed # Remove all bricks destroyed
+                if len(Breakout.bricks) == 0:
+                    self.loadNextLevel()
+                    continue
+
+
+                # Update the screen
+                pygame.display.flip() # Update the screen
+
+                # Hold key control
+                k = pygame.key.get_pressed()  #checking pressed keys
+                if k[pygame.K_LEFT] or k[pygame.K_a]: # arrow left
+                    Breakout.player.moveLeft()
+                elif k[pygame.K_RIGHT] or k[pygame.K_d]: # arrow right
+                    Breakout.player.moveRight()
             for event in pygame.event.get(): # for each event
                 if (event.type == pygame.ACTIVEEVENT and event.state == 2): # If change on the focus of the window
                     if event.gain == 0: # If focus lost
@@ -71,8 +121,7 @@ class Breakout():
                     elif event.key == 275 or event.key == 100: # Arrow right
                         Breakout.player.moveRight()
                     elif event.key == 110: # n pressed
-                        currentLvl = (currentLvl + 1) % 3 + 1
-                        # player, ball, bricks = loadLevel(currentLvl)
+                        self.loadNextLevel()
 
         print("\nThanks for playing, I hope you liked it.")
         print("See more projects like this one on https://github.com/jkutkut/")
